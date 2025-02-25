55e1472e1deb3b62b5d3dbc1277f6b5089589333
eneller <erikneller@gmx.de>
2025-02-25 14:09:28 +0100

    fix: crawl

--- src/epub2go/crawl.py
+++ src/epub2go/crawl.py
@@ -4,17 +4,16 @@ from tqdm import tqdm
 import os
 from urllib.parse import urljoin
 
-from convert import GBConvert
-import utils
+from convert import GBConvert, get_all_book_tags, allbooks_url
 
 def main():
-    books = utils.get_all_book_tags()
+    books = get_all_book_tags()
     # NOTE consider making this a map()
     for book in tqdm(books):
         book_title = book.get_text()
         book_url_relative = book.get('href')
         if book_url_relative is not None:
-            book_url = urljoin(allbooks_url, book_href)
+            book_url = urljoin(allbooks_url, book_url_relative)
             GBConvert(book_url).run()
 
 
