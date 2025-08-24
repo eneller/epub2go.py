55461a96021c9f7ab2ba46aa38080f164598f3ac
eneller <erikneller@gmx.de>
2025-08-24 23:50:51 +0200

    feat: cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+/// Download ePUBs from https://www.projekt-gutenberg.org/
+#[derive(Parser, Debug)]
+#[command(version)]
+struct Cli {
+    /// Set the log level to DEBUG
+    #[arg(short, long)]
+    debug: bool,
+
+    /// Disable the progress bar
+    #[arg(short, long)]
+    silent: bool,
+
+    /// The path to which files are saved
+    #[arg(short, long, default_value = ".")]
+    path: PathBuf,
+
+    /// Do not parse html files with blocklist
+    #[arg(long)]
+    no_clean: bool,
+
+    /// Number of concurrent download workers
+    #[arg(short = 'w', long, default_value_t = 10)]
+    max_workers: u8,
+
+    /// URLs to download from. If none are provided, enters interactive mode.
+    args: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
 }
